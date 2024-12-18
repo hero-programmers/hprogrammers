f5ec5a0218e89a6eee90ba3e66bd0ab7a815c72f
Nur Alam <nuralam.rsc@gmail.com>
2024-12-19 01:57:51 +0600

    chore(hero): clean unused import

--- src/sections/Headline.tsx
+++ src/sections/Headline.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { easeInOut, motion, useScroll, useTransform } from "framer-motion";
 
 const Headline = () => {
