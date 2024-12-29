f1e029775461a592d49472cb57161fdad685c147
Nur Alam <nuralam.rsc@gmail.com>
2024-12-29 17:07:13 +0600

    fix(theme-toggle): fix UI flickering

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -1,10 +1,18 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const ThemeButton = () => {
+  const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  // This is to prevent the UI from flickering
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
 
   return (
     <div className="flex items-center justify-center">
