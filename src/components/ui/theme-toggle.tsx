1d6a1458c1cd29e21af6b9138b07ff58fb6b9bee
Nur Alam <nuralam.rsc@gmail.com>
2024-12-29 17:37:37 +0600

    feat: add theme change transition

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -3,37 +3,56 @@
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const ThemeButton = () => {
   const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
+  const [isAnimating, setIsAnimating] = useState(false);
 
   // When mounted on client, now we can show the UI
   // This is to prevent the UI from flickering
   useEffect(() => setMounted(true), []);
 
+  const handleThemeChange = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setTheme(theme === "dark" ? "light" : "dark");
+      setIsAnimating(false);
+    }, 1000);
+  };
+
   if (!mounted) return null;
 
   return (
     <div className="flex items-center justify-center">
-      {theme === "dark" ? (
-        <button>
+      <button onClick={handleThemeChange}>
+        {theme === "dark" ? (
           <FaSun
             className="size-4"
             onClick={() => {
               setTheme("light");
             }}
           />
-        </button>
-      ) : (
-        <button>
+        ) : (
           <FaMoon
             className="size-4"
             onClick={() => {
               setTheme("dark");
             }}
           />
-        </button>
+        )}
+      </button>
+      {isAnimating && (
+        <>
+          <motion.div
+            className="fixed inset-0 z-[49] bg-gray-200 dark:bg-secondary"
+            initial={{ clipPath: "circle(150% at 50% 50%)" }}
+            animate={{ clipPath: "circle(0% at 50% 50%)" }}
+            exit={{ clipPath: "circle(0% at 50% 50%)" }}
+            transition={{ duration: 1 }}
+          />
+        </>
       )}
     </div>
   );
