dead15e338d21b658085cef66e92641e13f54a25
Nur Alam <nuralam.rsc@gmail.com>
2025-01-04 23:18:56 +0600

    fix(theme-transition): fix transition timeline, make responsive

--- src/components/animation/theme-transition.tsx
+++ src/components/animation/theme-transition.tsx
@@ -2,41 +2,53 @@
 import Logo from "@/assets/logo";
 import { useAnimate } from "framer-motion";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ThemeTransition = () => {
   const [scope, animate] = useAnimate();
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const logoWrapperRef = useRef<HTMLDivElement>(null);
+
   const { theme } = useTheme();
   const [pageTheme] = useState(theme);
   const backgroundColor =
     pageTheme === "dark" ? "bg-secondary" : "bg-background";
 
-  useEffect(() => {
-    const doAnimate = async () => {
-      try {
-        await animate(
-          "div",
-          { clipPath: "circle(150% at 50% 50%)" },
-          { duration: 1 },
-        );
+  const doAnimate = async () => {
+    if (!wrapperRef.current || !logoWrapperRef.current) return;
+    try {
+      await animate(
+        wrapperRef.current,
+        { clipPath: "circle(150% at 50% 50%)" },
+        { duration: 1 },
+      );
 
-        await animate("div", { scale: 6, x: 1920 * 1.5 }, { duration: 1 });
-        await animate(
-          "div",
-          { clipPath: "circle(0% at 50% 50%)" },
-          { duration: 1 },
-        );
-      } catch {}
-    };
+      await animate(
+        logoWrapperRef.current,
+        { scale: 8, x: 1920 * 2 },
+        { duration: 2 },
+      );
+      await animate(
+        wrapperRef.current,
+        { clipPath: "circle(0% at 150% 50%)" },
+        { duration: 1 },
+      );
+    } catch {}
+  };
+
+  useEffect(() => {
     doAnimate();
   });
 
   return (
     <div key={"theme-transition"} ref={scope}>
       <div
+        ref={wrapperRef}
         className={`fixed inset-0 flex items-center justify-center ${backgroundColor}`}
       >
-        <Logo />
+        <div ref={logoWrapperRef}>
+          <Logo className="size-64 md:size-full" />
+        </div>
       </div>
     </div>
   );
--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -4,25 +4,32 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 import ThemeTransition from "../animation/theme-transition";
+import { AnimatePresence } from "framer-motion";
 
 const ThemeButton = () => {
   const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
   const [isAnimating, setIsAnimating] = useState(false);
-  const DURATION = 3;
+  const [showIcon, setShowIcon] = useState(true);
+  const DURATION = 5;
 
   // When mounted on client, now we can show the UI
   // This is to prevent the UI from flickering
-  useEffect(() => setMounted(true), []);
+  useEffect(() => {
+    setMounted(true);
+    console.log("re--render");
+  }, []);
 
   const handleThemeChange = () => {
     setIsAnimating(true);
+    setShowIcon(false);
     setTimeout(() => {
       setTheme(theme === "dark" ? "light" : "dark");
     }, 1000);
 
     setTimeout(() => {
       setIsAnimating(false);
+      setShowIcon(true);
     }, DURATION * 1000);
   };
 
@@ -30,18 +37,22 @@ const ThemeButton = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <button onClick={handleThemeChange}>
-        {theme === "dark" ? (
-          <FaSun className="size-4" />
-        ) : (
-          <FaMoon className="size-4" />
-        )}
-      </button>
-      {isAnimating && (
-        <>
-          <ThemeTransition />
-        </>
+      {showIcon && (
+        <button onClick={handleThemeChange}>
+          {theme === "dark" ? (
+            <FaSun className="size-4" />
+          ) : (
+            <FaMoon className="size-4" />
+          )}
+        </button>
       )}
+      <AnimatePresence mode="wait" key="theme-transition">
+        {isAnimating && (
+          <>
+            <ThemeTransition />
+          </>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
