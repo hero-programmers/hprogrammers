3bc600950ed1d44b71a615f8b5c4d75bba80148c
Nur Alam <nuralam.rsc@gmail.com>
2025-01-05 02:41:20 +0600

    fix: add hover effect in navigation links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,13 +5,22 @@ import React from "react";
 const Navigation = () => {
   return (
     <>
-      <Link href="/" className="hover:text-gray-400">
+      <Link
+        href="/"
+        className="underline-offset-4 transition duration-500 hover:text-gray-600 hover:underline dark:hover:text-gray-400"
+      >
         Home
       </Link>
-      <Link href="/#about" className="hover:text-gray-400">
+      <Link
+        href="/#about"
+        className="underline-offset-4 transition duration-500 hover:text-gray-600 hover:underline dark:hover:text-gray-400"
+      >
         About
       </Link>
-      <Link href={discordInviteLink} className="hover:text-gray-400">
+      <Link
+        href={discordInviteLink}
+        className="underline-offset-4 transition duration-500 hover:text-gray-600 hover:underline dark:hover:text-gray-400"
+      >
         Join
       </Link>
     </>
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,9 +2,9 @@ import { Navbar } from "@/components";
 
 const Header = () => {
   return (
-    <header className="fixed w-full top-0 left-0 z-10 mt-5 px-4 md:px-0">
-      <div className="relative container p-4 text-foreground dark:text-background border  max-w-2xl dark:border-white/15 rounded-xl">
-        <div className="absolute inset-0 backdrop-blur -z-10 rounded-xl dark:bg-nav shadow" />
+    <header className="fixed left-0 top-0 z-10 mt-5 w-full px-4 md:px-0">
+      <div className="container relative max-w-2xl rounded-xl border p-4 text-foreground dark:border-white/15 dark:text-background">
+        <div className="absolute inset-0 -z-10 rounded-xl bg-white bg-opacity-70 shadow backdrop-blur dark:bg-nav" />
         <Navbar />
       </div>
     </header>
