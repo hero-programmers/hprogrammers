cc6710390a17e22e380011a7f701f5f99b5f3412
Nur Alam <nuralam.rsc@gmail.com>
2025-01-07 03:11:25 +0600

    chore: clean up header components

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,8 +1,7 @@
-import Header from "./Header";
 import Hero from "./Hero";
 import About from "./About";
 import Headline from "./Headline";
 import CallToAction from "./CallToAction";
 import Footer from "./Footer";
 
-export { Hero, Headline, CallToAction, Footer, Header, About };
+export { Hero, Headline, CallToAction, Footer, About };
