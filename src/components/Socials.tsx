3796beb40616d6da01732a61c626f6f77aac4355
Nur Alam <nuralam.rsc@gmail.com>
2025-01-08 21:17:44 +0600

    fix: fix footer social links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,30 +1,20 @@
-import { FaDiscord, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { socialLinks } from "@/constant";
 
 const Socials = () => {
   return (
     <div>
       <h4 className="border-b py-2 text-lg font-medium">Socials</h4>
       <ul className="mx-4 my-4 list-inside space-y-1 text-discord-white-600 *:w-fit *:transition *:duration-300">
-        <li className="list-item underline-offset-2 *:flex *:items-center *:gap-2 *:font-medium hover:text-white hover:underline">
-          <a href="" target="_blank ">
-            <FaYoutube /> Youtube
-          </a>
-        </li>
-        <li className="list-item underline-offset-2 *:flex *:w-fit *:items-center *:gap-2 *:font-medium hover:text-white hover:underline">
-          <a href="" target="_blank">
-            <FaFacebook /> Facebook
-          </a>
-        </li>
-        <li className="list-item underline-offset-2 *:flex *:w-fit *:items-center *:gap-2 *:font-medium hover:text-white hover:underline">
-          <a href="" target="_blank">
-            <FaInstagram /> Instagram{" "}
-          </a>
-        </li>
-        <li className="list-item underline-offset-2 *:flex *:w-fit *:items-center *:gap-2 *:font-medium hover:text-white hover:underline">
-          <a href="" target="_blank">
-            <FaDiscord /> Discord
-          </a>
-        </li>
+        {socialLinks.map((link) => (
+          <li
+            key={link.name}
+            className="list-item underline-offset-2 *:flex *:items-center *:gap-2 *:font-medium hover:text-white hover:underline"
+          >
+            <a href={link.href} target="_blank ">
+              <link.icon /> {link.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,29 @@
+import { FaDiscord, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+
 export const headline = "Let's code_ your career & learn as one.";
 
 export const discordInviteLink =
   "https://discord.gg/hero-programmers-720365448809545742";
+
+export const socialLinks = [
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/@hprogrammers",
+    icon: FaYoutube,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/hprogrammer",
+    icon: FaFacebook,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/heroprogrammers",
+    icon: FaInstagram,
+  },
+  {
+    name: "Discord",
+    href: discordInviteLink,
+    icon: FaDiscord,
+  },
+];
