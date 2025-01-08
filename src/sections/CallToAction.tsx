5ec9ea39450b5012e5bd1e8dc06c5bd9e6030dcf
Nur Alam <nuralam.rsc@gmail.com>
2025-01-08 21:06:32 +0600

    feat(call-to-action): add fade in animaiton to call to action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -7,6 +7,7 @@ import useMousePosition from "@/hooks/useMousePosition";
 import { motion, useMotionTemplate } from "framer-motion";
 import { DiscordLogoFloating } from "@/components/ui";
 import { TextSlideUp } from "@/components/animation/text-popup";
+import { FadeIn } from "@/components/animation";
 
 const CallToAction = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -23,29 +24,31 @@ const CallToAction = () => {
           So, what are you waiting for? Join us today!
         </TextSlideUp>
       </h2>
-      <div
-        ref={containerRef}
-        className="relative mx-2 flex h-screen items-center justify-center overflow-hidden rounded-xl border border-slate-400/50 md:mx-8"
-      >
-        {/*<div className="absolute inset-0 rounded-2xl bg-primary bg-[repeating-linear-gradient(transparent_0%_49%,var(--foreground)_49%_50%,transparent_50%),repeating-linear-gradient(90deg,transparent_0%_49%,var(--foreground)_49%_50%,transparent_50%)] [background-size:50px_50px] dark:bg-[repeating-linear-gradient(transparent_0%_49%,var(--background)_49%_50%,transparent_50%),repeating-linear-gradient(90deg,transparent_0%_49%,var(--background)_49%_50%,transparent_50%)] dark:opacity-30" />*/}
-        <motion.div
-          style={{ backgroundImage }}
-          className="absolute inset-0 rounded-xl bg-hero"
-        />
-        <DiscordLogoFloating />
-        <DiscordLogoFloating className="bottom-0 left-full -translate-x-1/2 translate-y-16 rotate-[120deg]" />
-        <div className="z-[2]">
-          <p className="text-4xl text-white md:text-6xl lg:text-8xl">
-            Join us on{" "}
-            <Link
-              href={discordInviteLink}
-              className="underline underline-offset-2"
-            >
-              Discord.
-            </Link>
-          </p>
+      <FadeIn>
+        <div
+          ref={containerRef}
+          className="relative mx-2 flex h-screen items-center justify-center overflow-hidden rounded-xl border border-slate-400/50 md:mx-8"
+        >
+          {/*<div className="absolute inset-0 rounded-2xl bg-primary bg-[repeating-linear-gradient(transparent_0%_49%,var(--foreground)_49%_50%,transparent_50%),repeating-linear-gradient(90deg,transparent_0%_49%,var(--foreground)_49%_50%,transparent_50%)] [background-size:50px_50px] dark:bg-[repeating-linear-gradient(transparent_0%_49%,var(--background)_49%_50%,transparent_50%),repeating-linear-gradient(90deg,transparent_0%_49%,var(--background)_49%_50%,transparent_50%)] dark:opacity-30" />*/}
+          <motion.div
+            style={{ backgroundImage }}
+            className="absolute inset-0 rounded-xl bg-hero"
+          />
+          <DiscordLogoFloating />
+          <DiscordLogoFloating className="bottom-0 left-full -translate-x-1/2 translate-y-16 rotate-[120deg]" />
+          <div className="z-[2]">
+            <p className="text-4xl text-white md:text-6xl lg:text-8xl">
+              Join us on{" "}
+              <Link
+                href={discordInviteLink}
+                className="underline underline-offset-2"
+              >
+                Discord.
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
+      </FadeIn>
     </section>
   );
 };
