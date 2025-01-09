77978c8c4fb98cba95dd22bf8d2afec4e6005487
Nur Alam <nuralam.rsc@gmail.com>
2025-01-09 18:50:37 +0600

    docs: add metadata

---
+++ src/constant/metadata.ts
@@ -0,0 +1,28 @@
+export const meta = {
+  author: {
+    name: "Nur Alam",
+    username: "nuralam123",
+  },
+  site: {
+    title: "Hero Programmers",
+    description:
+      "We, a friendly community for beginner programmers who wants to become a PROGRAMMING HERO! So, Just join us on discord.",
+    bannerImage: "/hero-programmers.jpg",
+    url: "https://hero-programmers.vercel.app",
+    keywords: [
+      "heroprogrammers",
+      "coding",
+      "community",
+      "programming",
+      "programming hero",
+      "hero programmers",
+      "coding resources",
+      "discord",
+      "discord server",
+      "discord programming server",
+      "hp",
+    ],
+  },
+  language: "en",
+  charset: "UTF-8",
+};
